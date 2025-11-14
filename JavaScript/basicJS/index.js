805ed68e5420884d7b4d3fa363e05bacf97de4bd
Adnankhan15929 <khanadnan0339@gmail.com>
2025-11-14 20:28:25 +0530

    java script

---
+++ JavaScript/basicJS/index.js
@@ -0,0 +1,21 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10;
+
+console.log(a, b);
+
+let ar = ["abc", 123, "!@#", "a", 234, "543", "True"];
+console.log(ar);
+
+alert("this is alert")
+
+console.warn("This is warning");
+console.error("ye error hai");
+
+if (confirm("are you sure")) {
+  console.log("thanks");
+} else {
+  console.error("it is alert");
+}
