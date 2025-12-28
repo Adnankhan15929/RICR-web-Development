464724f3a3fa8e9363980b7e4591fb01f4bd8566
Adnankhan15929 <khanadnan0339@gmail.com>
2025-12-29 00:18:00 +0530

    refactor: comment out unused components and styles; enhance Home page layout and content

--- React/MakeupApi/src/pages/About.jsx
+++ React/MakeupApi/src/pages/About.jsx
@@ -1,98 +1,109 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
+
+// const About = () => {
+//   const [fullName, setFullName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [contact, setContact] = useState("");
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const handleClearForm = () => {
+//     setFullName("");
+//     setEmail("");
+//     setContact("");
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     setIsLoading(true);
+//     try {
+//       const response = await fetch(
+//         "https://jsonplaceholder.typicode.com/posts"
+//       );
+//       const data = {
+//         fullName,
+//         email,
+//         contact,
+//       };
+//       console.log("Form Data Submitted:", data);
+//     } catch (error) {
+//       console.log(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+
+//     handleClearForm();
+//   };
+
+//   return (
+//     <>
+//       <div className="text-center px-96 text-neutral-700 font-family: 'Abel', sans-serif ">
+//         <h1 className="text-4xl pb-5">Contact Us</h1>
+//         <div htmlFor="container text-xl">
+//           <form onReset={handleClearForm} onSubmit={handleSubmit}>
+//             <div>
+//               <label htmlFor="fullName">Full Name: </label>
+//               <input
+//                 type="text"
+//                 name="fullName"
+//                 className="border m-3 rounded p-1 text-blue-900"
+//                 value={fullName}
+//                 onChange={(event) => setFullName(event.target.value)}
+//                 placeholder="Enter your name"
+//                 required
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="email">Email:</label>
+//               <input
+//                 type="email"
+//                 name="email"
+//                 className="border m-3 rounded p-1 text-blue-900"
+//                 value={email}
+//                 onChange={(event) => setEmail(event.target.value)}
+//                 placeholder="Enter your email"
+//                 required
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="contact">Contact:</label>
+//               <input
+//                 type="number"
+//                 name="contact"
+//                 className="border m-3 rounded p-1 text-blue-900"
+//                 value={contact}
+//                 onChange={(event) => setContact(event.target.value)}
+//                 placeholder="Enter your phone no."
+//                 required
+//               />
+//             </div>
+//             <button
+//               className=" bg-amber-400 border border-black me-3 rounded p-1"
+//               type="submit"
+//             >
+//               {isLoading ? "Loading..." : "Submit"}
+//             </button>
+//             <button
+//               className=" bg-blue-700 border border-black rounded p-1"
+//               type="reset"
+//             >
+//               Reset
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default About;
 
 const About = () => {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [contact, setContact] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleClearForm = () => {
-    setFullName("");
-    setEmail("");
-    setContact("");
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-    try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-        const data = {
-          fullName,
-          email,
-          contact,
-        };
-        console.log("Form Data Submitted:", data);
-    } catch (error) {
-      console.log(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-
-    handleClearForm();
-  };
-
-  return (
-    <>
-      <div className="text-center px-96 text-neutral-700 font-family: 'Abel', sans-serif ">
-        <h1 className="text-4xl pb-5">Contact Us</h1>
-        <div htmlFor="container text-xl">
-          <form onReset={handleClearForm} onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="fullName">Full Name: </label>
-              <input
-                type="text"
-                name="fullName"
-                className="border m-3 rounded p-1 text-blue-900"
-                value={fullName}
-                onChange={(event) => setFullName(event.target.value)}
-                placeholder="Enter your name"
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                name="email"
-                className="border m-3 rounded p-1 text-blue-900"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="contact">Contact:</label>
-              <input
-                type="number"
-                name="contact"
-                className="border m-3 rounded p-1 text-blue-900"
-                value={contact}
-                onChange={(event) => setContact(event.target.value)}
-                placeholder="Enter your phone no."
-                required
-              />
-            </div>
-            <button
-              className=" bg-amber-400 border border-black me-3 rounded p-1"
-              type="submit"
-            >
-              {isLoading ? "Loading..." : "Submit"}
-            </button>
-            <button
-              className=" bg-blue-700 border border-black rounded p-1"
-              type="reset"
-            >
-              Reset
-            </button>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+  return 
+  <>
+  
+  </>;
 };
 
 export default About;
--- React/MakeupApi/src/pages/Home.jsx
+++ React/MakeupApi/src/pages/Home.jsx
@@ -9,27 +9,55 @@ import img5 from "../assets/img5.png";
 const Home = () => {
   return (
     <>
-      <div className=" px-85 text-neutral-700" >
-        <h1 className="pt-12 text-5xl text-center">Makeup API</h1>
-        <p className="py-12 ">
+      <div className=" px-75 pt-15 text-neutral-700">
+        <h1 className=" text-5xl text-center font-stretch-50% " id="makeUpH">
+          Makeup API
+        </h1>
+        <p className="py-10 text-left text-sm">
           Welcome to the Makeup API. Explore our collection of makeup products
           and brands. Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Veniam inventore rerum iste sint veritatis.
         </p>
       </div>
-      <div className=" flex gap-1 align-center justify-center"> 
-        <div className = "grid grid-rows-2 grid-cols-1 gap-2 ">
-          <img src={img1} alt="i1" width="380px"/>
-          <img src={img2} alt="i2" width="380px"/>
+      <div className="px-35 flex gap-1 align-center justify-center">
+        <div className="grid grid-rows-2 grid-cols-1 gap-2 ">
+          <img src={img1} alt="i1" width="440px" />
+          <img src={img2} alt="i2" width="440px" />
         </div>
-        <div className = "grid grid-rows-1 grid-cols-1 px-1">
-          <img src={img3} alt="i3" width="380px"/>
-
+        <div className="grid grid-rows-1 grid-cols-1 px-1">
+          <img src={img3} alt="i3" width="440px" />
         </div>
-        <div className = "grid grid-rows-2 grid-cols-1 gap-2">
-          <img src={img4} alt="i4" width="380px"/>
-          <img src={img5} alt="i5" width="380px"/>
+        <div className="grid grid-rows-2 grid-cols-1 gap-2">
+          <img src={img4} alt="i4" width="440px" />
+          <img src={img5} alt="i5" width="440px" />
         </div>
+      </div>
+      <div className="px-35 mt-15 text-2xl ">
+        <h2>About the Makeup API</h2>
+        <hr />
+        <ul className="list-disc p-15 ps-20 text-lg">
+          <li className="pb-9 ">
+            <span className="font-semibold">Protocol:</span>The transport is
+            HTTP. This API conforms to REST principals
+          </li>
+          <li className="pb-9">
+            <span className="font-semibold">API Endpoints:</span>The endpoint
+            for the current API version is: <br /><br />
+            <span className="ps-5 underline text-amber-900">https://makeup-api.herokuapp.com/api/v1/products.json</span>
+          </li>
+          <li className="pb-9">
+            <span className="font-semibold">
+              There is currently one API call:
+            </span>
+            Search makeup products
+          </li>
+          <li className="pb-9">
+            <span className="font-semibold">Type:</span>Get
+          </li>
+          <li className="pb-9">
+            <span className="font-semibold">Response Format:</span>json
+          </li>
+        </ul>
       </div>
     </>
   );
