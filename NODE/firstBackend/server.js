27cf5bac19812cce881ba0b9dc3421d47499b2bb
Adnankhan15929 <khanadnan0339@gmail.com>
2026-01-09 21:07:55 +0530

    feat: add user authentication with mongoose and nodemon
    - Updated package.json to include mongoose and nodemon as dependencies.
    - Implemented MongoDB connection in a new dataBase.js file.
    - Created user model (userModel.js) for user data schema.
    - Developed user registration, login, and logout functionalities in myController.js.
    - Set up routes for user authentication in myRouter.js.
    - Modified server.js to use express.json() and added authentication routes.

--- NODE/firstBackend/server.js
+++ NODE/firstBackend/server.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import connectDB from "./src/config/dataBase.js";
+import AuthRouter from "./src/routers/myRouter.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/auth", AuthRouter);
 
 app.get("/", (req, res) => {
   console.log("Server is Running");
@@ -12,4 +18,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server Started at port", port);
+  connectDB();
 });
