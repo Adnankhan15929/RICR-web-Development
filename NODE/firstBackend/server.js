cfe55534e0c63a83f4b857eed66931d3a31d3ec6
Adnankhan15929 <khanadnan0339@gmail.com>
2026-01-10 20:34:34 +0530

    middleware and update user

--- NODE/firstBackend/server.js
+++ NODE/firstBackend/server.js
@@ -2,21 +2,26 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./src/config/dataBase.js";
-import AuthRouter from "./src/routers/myRouter.js";
-
+import AuthRouter from "./src/routers/myRouter.js"
 const app = express();
 
 app.use(express.json());
-
-app.use("/auth", AuthRouter);
+app.use("/auth",AuthRouter);
 
 app.get("/", (req, res) => {
-  console.log("Server is Running");
-  res.json({ message: "Server is Running Successfully" });
+  console.log("server is running");
+  res.json({ message: "server is running successfully" });
 });
+
+app.use((err,req,res,next)=>{
+  const ErrorMessage =  err.message ||"Internal server error";
+  const StatusCode=err.statusCode ||500;
+
+  res.status(StatusCode).json({message:ErrorMessage});
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server Started at port", port);
+  console.log("server started at port", port);
   connectDB();
 });
