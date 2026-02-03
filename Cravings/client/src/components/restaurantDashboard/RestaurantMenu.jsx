c828fa646902cc71126f910682b6a86fe24c3408
Adnankhan15929 <khanadnan0339@gmail.com>
2026-02-03 16:13:49 +0530

    feat: Add AddMenuItemModal component for menu item management; implement form handling and validation

--- Cravings/client/src/components/restaurantDashboard/RestaurantMenu.jsx
+++ Cravings/client/src/components/restaurantDashboard/RestaurantMenu.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import AddMenuItemModal from "./modals/AddMenuItemModal";
 
 const RestaurantMenu = () => {
+  const [isAddItemModalOpen, setIsAddItemModalOpen] = React.useState(false);
   return (
-    <div className="bg-gray-50 rounded-lg p-6 h-full overflow-y-auto">
-      <div className="bg-white rounded-lg shadow-md p-6 border border-gray-200">
-        <div className="flex justify-between items-center"><h2 className="text-2xl font-bold text-gray-800 mb-4">Menu Management</h2>
-        <button className="text-lg cursor-pointer font-semibold  text-white bg-(--color-secondary) w-fit rounded px-3 py-2">+Add Items</button></div>
-        <div className="text-center text-gray-500 py-12">
-          <p className="text-lg">Menu items will be displayed and managed here</p>
+    <>
+      <div className="bg-gray-50 rounded-lg p-6 h-full overflow-y-auto">
+        <div className="bg-white rounded-lg shadow-md p-6 border border-gray-200 flex justify-between">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+            Menu Management
+          </h2>
+          <button
+            className="px-4 py-2 bg-(--color-secondary) text-white rounded-lg hover:bg-(--color-secondary-hover) transition font-semibold"
+            onClick={() => setIsAddItemModalOpen(true)}
+          >
+            Add Item
+          </button>
         </div>
       </div>
-    </div>
+
+      {isAddItemModalOpen && (
+        <AddMenuItemModal onClose={() => setIsAddItemModalOpen(false)} />
+      )}
+    </>
   );
 };
 
