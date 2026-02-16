5370c7f46db93d06528c5ea3e5ab9f44d432af5f
Adnankhan15929 <khanadnan0339@gmail.com>
2026-02-16 13:13:11 +0530

    feat: Add initial HTML, CSS, and JavaScript files for GSAP animation; create a box element with animation properties

---
+++ GSAP/script.js
@@ -0,0 +1,14 @@
+gsap.to("#box",{
+    x:1000,
+    y:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    scale:2,
+    backgroundColor:"red",
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true
+
+})
+
