842df332474d19473117a611ecd369d768be301f
Adnankhan15929 <khanadnan0339@gmail.com>
2026-02-18 03:13:52 +0530

    feat: Refactor HTML structure and CSS styles; enhance SVG integration and improve layout for page sections

--- GSAP/script.js
+++ GSAP/script.js
@@ -53,25 +53,72 @@
 //     }
 // })
 
-gsap.from("#page2 h1",{
-    opacity:0,
-    duration:2,
-    x:500,
-    scrollTrigger:{
-        trigger:"#page2 h1",
-        scroller:"body",
-        markers:true,
-        start:"top 50%"
-    }
-})
-gsap.from("#page2 h2",{
-    opacity:0,
-    duration:2,
-    x:-500,
-    scrollTrigger:{
-        trigger:"#page2 h2",
-        scroller:"body",
-        markers:true,
-        start:"top 50%"
-    }
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+// })
+// gsap.from("#page2 h2",{
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+// })
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     opacity:0,
+//     rotate:720,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%",
+//         end:"top 30%",
+//         scrub:2,
+//         pin:true,
+//     }
+// })
+
+
+
+
+// gsap.to("#page2 h1",{
+//     Transform:"translateX(-150%)",
+//     scrollTrigger:{
+//         trigger:"#page2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 0%",
+//         end:"top -150%",
+//         scrub:2,
+//         pin:true,
+
+//     }
+// })
+
+
+
+
+
+var path = `M 10 100 Q 250 100 490 100`
+
+var finalPath = `M 10 100 Q 250 100 490 100`
+
+var string = document.querySelector("#string")
+
+string.addEventListener("mouseenter",function(dets){
+    path = `M 10 100 Q 250 100 490 100`
 })
