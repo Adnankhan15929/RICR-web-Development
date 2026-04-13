979c7178c46727f55ddc5fd492f367072d1f3fc9
Adnankhan15929 <khanadnan0339@gmail.com>
2026-04-13 15:46:33 +0530

    feat: add new HTML and JavaScript files for Day 8 exercises with variable display

---
+++ WebDTraining/Day_8/first.js
@@ -0,0 +1,11 @@
+// var a = 10;
+// document.getElementById('p1').innerHTML=a;
+
+
+var Name = "Adnan khan";
+var age = 18;
+document.getElementById('name').innerHTML="Name :- "+Name;
+document.getElementById('age').innerHTML="Age :- "+age;
+console.log(Name);
+console.log(age);
+
