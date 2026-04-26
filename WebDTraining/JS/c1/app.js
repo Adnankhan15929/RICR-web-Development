32c523cf1a9bc30254fc8a35c52fef3fc115fe2d
Adnankhan15929 <khanadnan0339@gmail.com>
2026-04-27 01:22:11 +0530

    Refactor app.js by commenting out unused code and adding new array manipulation examples

--- WebDTraining/JS/c1/app.js
+++ WebDTraining/JS/c1/app.js
@@ -20,21 +20,111 @@
 // let age = prompt("Enter your age:");
 // alert(name+" is "+age+" years old.");
 
-let q=Number(prompt("Enter your quarter:"));
-switch(q){
-    case 1:
-        console.log("j, f, m");
-        break
-    case 2:
-        console.log("amj");
-        break;
-    case 3:
-        console.log("jas");
-        break;
-    case 4:
-        console.log("ond");
-        break;
-    default:
-        console.log("invalid");
-        break;
-}
+// let q=Number(prompt("Enter your quarter:"));
+// switch(q){
+//     case 1:
+//         console.log("j, f, m");
+//         break
+//     case 2:
+//         console.log("amj");
+//         break;
+//     case 3:
+//         console.log("jas");
+//         break;
+//     case 4:
+//         console.log("ond");
+//         break;
+//     default:
+//         console.log("invalid");
+//         break;
+// }
+
+// let h = "       jaskfjaskdlj            ";
+// console.log(h);
+// console.log(typeof h);
+// h=h.trim();
+// console.log(h);
+// console.log(typeof h);
+
+// let n="adnan khan";
+// console.log(n);
+// n=n.toUpperCase();
+// console.log(n);
+// n=n.toLowerCase();
+// console.log(n);
+
+// let a="welcome";
+// console.log(a.indexOf("l"));
+// console.log(a.indexOf("p"));
+
+// let k="     asdfsdf asdfasd      ";
+// console.log(k.toUpperCase().trim());
+
+// let para = "adnan";
+// console.log(para.slice(0));
+// console.log(para.slice(2));
+// console.log(para.slice(-1));
+// console.log(para.slice(-3,5));
+// console.log(para.slice(3,5));
+
+// let name="Adnan khan";
+// console.log(name.replace("khan","pahtan"));
+// console.log(name.replace("k","p"));
+// console.log(name.repeat(3));
+
+// let arr = [1,23,4];
+// console.log(arr);
+
+// let cars = ["bmw","royalenfield"];
+// cars.push("hmalaena");
+// console.log(cars);
+// cars.pop();
+// console.log(cars);
+// cars.unshift("bansdnas");
+// console.log(cars);
+// cars.shift();
+// console.log(cars);
+
+// let p = ["323","adnan","234",23];
+// console.log(p.includes(323));
+// console.log(p.includes("adnan"));
+
+// let p = ["323","adnan","234",23];
+// let d=[23,34,12,"nsadf","wer"];
+// console.log(p.concat(d));
+// console.log(d.reverse());
+// console.log(d);
+
+// let colors=["red","yellow","black"];
+// console.log(colors.splice(1));
+// console.log(colors);
+// colors=["red","yellow","black"];
+// console.log(colors.splice(0,1));
+// console.log(colors);
+// console.log(colors.splice(1,2));
+// console.log(colors);
+// colors=["red","yellow","black"];
+// console.log(colors.splice(1,2,"asdfas","asdfasdf"));
+// console.log(colors);
+// console.log(colors.splice(1,0,"mercedes"));
+// console.log(colors);
+// console.log(colors.splice(1,1,"BMW"));
+// console.log(colors);
+
+// let d=["red","yellow","black"];
+// console.log(d); 
+// d.sort();
+// console.log(d);
+
+// let pq = ["jan","jul","mar","aug"];
+// console.log(pq);
+// pq.splice(0,2, "july","june");
+// console.log(pq);
+
+// let lan= ["c","c++","hmtl","javascrit","python","java"];
+// console.log(lan.reverse().indexOf("javascrit"));
+
+// let arr = [["x",null,"0"],[null,"x",null],["0",null,"x"]];
+// console.log(arr);
+// arr[0][1]="0";
+// console.log(arr);
