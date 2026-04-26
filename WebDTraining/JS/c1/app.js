e4643d2806053fe0fa0c108fa3dd4e305265155b
Adnankhan15929 <khanadnan0339@gmail.com>
2026-04-26 23:49:26 +0530

    Add initial HTML and JavaScript files for quarter selection functionality

---
+++ WebDTraining/JS/c1/app.js
@@ -0,0 +1,40 @@
+// let pencil = 10;
+// let pen = 12;
+// // console.log("the total price is "+pencil+pen);
+// let output = `The total is : ${pencil+pen} Rupees.`;
+// console.log(output);
+
+// alert("asjdfjasdkfj");
+// console.error("sdafasdfasiufo");
+// console.warn("sdafasdfasiufo");
+
+// let name = prompt("Enter your name: ");
+// console.log(name);
+
+// let first = prompt("Enter your name:");
+// console.log("Welcome"+first);
+// console.error("Error"+first);
+// console.error("Error",first);
+
+// let name = prompt("Enter your name:");
+// let age = prompt("Enter your age:");
+// alert(name+" is "+age+" years old.");
+
+let q=Number(prompt("Enter your quarter:"));
+switch(q){
+    case 1:
+        console.log("j, f, m");
+        break
+    case 2:
+        console.log("amj");
+        break;
+    case 3:
+        console.log("jas");
+        break;
+    case 4:
+        console.log("ond");
+        break;
+    default:
+        console.log("invalid");
+        break;
+}
