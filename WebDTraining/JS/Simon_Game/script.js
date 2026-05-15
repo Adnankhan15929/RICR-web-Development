f4d0814ad566043305e88fa3753da58d3239b1b5
Adnankhan15929 <khanadnan0339@gmail.com>
2026-05-15 23:44:21 +0530

    no change

--- WebDTraining/JS/Simon_Game/script.js
+++ WebDTraining/JS/Simon_Game/script.js
@@ -2,8 +2,8 @@ let gameSeq = [];
 let userSeq = [];
 let started = false;
 let level = 0;
-let btns = ['yellow','red','purple','black']
-let h2 = document.querySelector('h2');
+let btns = ["yellow", "red", "purple", "black"];
+let h2 = document.querySelector("h2");
 document.addEventListener("keypress", function () {
   if (started == false) {
     console.log("game started");
@@ -12,32 +12,33 @@ document.addEventListener("keypress", function () {
   }
 });
 
-function levelUp(){
-    level++;
-    h2.innerText = `Level ${level}`;
-    let randIdx = Math.floor(Math.random*3);
-    let randcolor = btns[randIdx];
-    let randbtn = document.querySelector(`.${randcolor}`)
-    gameFlash(randbtn);
+function levelUp() {
+  level++;
+  h2.innerText = `Level ${level}`;
+  let randIdx = Math.floor(Math.random * 3);
+  let randcolor = btns[randIdx];
+  let randbtn = document.querySelector(`.${randcolor}`);
+  gameFlash(randbtn);
 }
-function btnPress(){
-    let btn = this;
-    console.log(this);
-    userFlash(btn);
+
+function btnPress() {
+  let btn = this;
+  console.log(this);
+  userFlash(btn);
 }
-let allBtns = document.querySelectorAll('.btn');
-for(btn of allBtns){
-    btn.addEventListener('click',btnPress);
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+  btn.addEventListener("click", btnPress);
 }
-function gameFlash(btn){
-    btn.classList.add('flash');
-    setTimeout(function(){
-        btn.classList.remove('flash');
-    },250);
+function gameFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
 }
-function userFlash(){
-    btn.classList.add('userflash');
-    setTimeout(function (){
-        btn.classList.remove('userflash')
-    },250)
+function userFlash() {
+  btn.classList.add("userflash");
+  setTimeout(function () {
+    btn.classList.remove("userflash");
+  }, 250);
 }
