ad8ef7eb2e7363d927c9a0a2d1a79a825435aad5
Adnankhan15929 <khanadnan0339@gmail.com>
2026-05-17 16:17:18 +0530

    Refactor Simon Game: Update button IDs, fix button colors, and enhance game logic

--- WebDTraining/JS/Simon_Game/script.js
+++ WebDTraining/JS/Simon_Game/script.js
@@ -2,7 +2,7 @@ let gameSeq = [];
 let userSeq = [];
 let started = false;
 let level = 0;
-let btns = ["yellow", "red", "purple", "black"];
+let btns = ["yellow", "red", "purple", "pink"];
 let h2 = document.querySelector("h2");
 document.addEventListener("keypress", function () {
   if (started == false) {
@@ -11,33 +11,58 @@ document.addEventListener("keypress", function () {
     levelUp();
   }
 });
-
-function levelUp() {
-  level++;
-  h2.innerText = `Level ${level}`;
-  let randIdx = Math.floor(Math.random * 3);
-  let randcolor = btns[randIdx];
-  let randbtn = document.querySelector(`.${randcolor}`);
-  gameFlash(randbtn);
-}
-function btnPress() {
-  let btn = this;
-  console.log(this);
-  userFlash(btn);
-}
-let allBtns = document.querySelectorAll(".btn");
-for (btn of allBtns) {
-  btn.addEventListener("click", btnPress);
-}
 function gameFlash(btn) {
   btn.classList.add("flash");
   setTimeout(function () {
     btn.classList.remove("flash");
   }, 250);
 }
-function userFlash() {
+function userFlash(btn) {
   btn.classList.add("userflash");
   setTimeout(function () {
     btn.classList.remove("userflash");
   }, 250);
+}
+
+function checkAns(idx) {
+  if (gameSeq[idx] === userSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press any key to start again`;
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout(function() {
+       document.querySelector("body").style.backgroundColor="white";
+    }, 150);
+    reset();
+  }
+}
+function levelUp() {
+  userSeq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
+  let randIdx = Math.floor(Math.random() * 3);
+  let randcolor = btns[randIdx];
+  let randbtn = document.querySelector(`.${randcolor}`);
+  gameSeq.push(randcolor);
+  gameFlash(randbtn);
+}
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+  btn.addEventListener("click", btnPress);
+}
+function btnPress() {
+  let btn = this;
+  let userColor = btn.getAttribute("id"); 
+  userSeq.push(userColor);
+  userFlash(btn);
+  checkAns(userSeq.length - 1);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
 }
